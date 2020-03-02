15d66467206ad9e077b4adac57af2024b344d57b
William Kennedy <alexkenned@gmail.com>
2020-03-02 19:23:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,40 +1,16 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {
-  "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-}
-
+  i = 0 
+  new_hash = {}
+  while i < data[:gender][i] do 
+    c = 0 
+    gender = data[:gender][i]
+    
+    while c < data[:gender][i][c] do 
+      name = data[:gender][i][c]
+      new_hash["#{name}"] = {}
+      c+=1 
+    end 
+    new_hash[name][gender] = gender 
+    i+=1 
+    
 end
