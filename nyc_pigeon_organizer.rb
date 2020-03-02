d2f8915ea0288c00b2b47279706e1bae9821795f
William Kennedy <alexkenned@gmail.com>
2020-03-02 19:26:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,7 @@ def nyc_pigeon_organizer(data)
   i = 0 
   new_hash = {}
   while i < data[:gender].length do 
-    data.map |data| {new_hash[:gender][i] = [:gender]
+    data.map |data| {new_hash[:gender][i] = [:gender]}
+  i+=1 
+  end
 end
